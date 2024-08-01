7f6e07d605781518a2e37f2944e891feff329db0
hchae31 <chaeheegon5858@gmail.com>
2024-08-01 11:11:45 +0900

    css3 - 포지션

--- app/routes/css.py
+++ app/routes/css.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from starlette.templating import Jinja2Templates
 
 # 라우터 생성
@@ -7,3 +7,30 @@ css_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
 
 # 라우트 설정
+@css_router.get('/')
+async def index(req: Request):
+    return templates.TemplateResponse('css/index.html', {'request': req})
+
+@css_router.get('/selector') # /select = /css/select
+async def selector(req: Request):
+    return templates.TemplateResponse('css/01selector.html', {'request': req})
+
+@css_router.get('/reset')
+async def reset(req: Request):
+    return templates.TemplateResponse('css/02reset.html', {'request': req})
+
+@css_router.get('/text')
+async def text(req: Request):
+    return templates.TemplateResponse('css/03text.html', {'request': req})
+
+@css_router.get('/box')
+async def text(req: Request):
+    return templates.TemplateResponse('css/04box.html', {'request': req})
+
+@css_router.get('/list')
+async def text(req: Request):
+    return templates.TemplateResponse('css/05list.html', {'request': req})
+
+@css_router.get('/position')
+async def text(req: Request):
+    return templates.TemplateResponse('css/06position.html', {'request': req})
